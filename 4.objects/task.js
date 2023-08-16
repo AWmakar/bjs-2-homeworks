9cec85bd596ca0210c42e8554487aeb41eabf070
zaidstewart <zaidstewart@iomail.one>
2023-08-16 17:57:05 +0300

    Add objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
+
+
 function Student(name, gender, age) {
   
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
   
+    this.subject = subjectName;
+
 }
 
 Student.prototype.addMarks = function (...marks) {
   
+    if (!this.marks)
+    {
+        return;
+    }
+
+    this.marks.push(...marks);
+
 }
 
 Student.prototype.getAverage = function () {
+
+    if (!this.marks || !this.marks.length)
+    {
+        return 0;
+    }
   
+    let result = this.marks.reduce((acc, item) => acc + item, 0);
+    return result / this.marks.length;
+
 }
 
 Student.prototype.exclude = function (reason) {
+
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
   
 }
